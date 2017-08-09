1856eb4ebf33085286f357894c235d1f60d112f5
Kostis Pristouris <kprist@gmail.com>
2017-08-09 16:58:42 +0300

    Fix microservices configuration

---
+++ application/configs/microservices.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const co = require('../common');
+
+module.exports = {
+    'deck': {
+        uri: (!co.isEmpty(process.env.SERVICE_URL_DECK)) ? process.env.SERVICE_URL_DECK : 'http://deckservice',
+    },
+};
--- application/configs/microservices.sample.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-
-module.exports = {
-    'deck': {
-        uri: 'http://deckservice.experimental.slidewiki.org',
-    }
-};
